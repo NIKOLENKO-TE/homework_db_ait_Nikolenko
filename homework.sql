4589ea17bb5a5637e723be942ed0aa53dbdc260d
Tymofii Nikolenko <133020063+NIKOLENKO-TE@users.noreply.github.com>
2023-10-10 23:36:59 +0200

    Add files via upload

---
+++ homework.sql
@@ -0,0 +1,22 @@
+-- Задача 2. Вывести название и стоимость товаров от 20 EUR.
+	SELECT ProductName, Price
+	FROM [Products]
+	WHERE Price >20
+	ORDER BY Price ASC 
+
+-- Задача 3. Вывести страны поставщиков.
+	SELECT Country 
+	FROM [Suppliers]
+
+-- Задача 4. В упорядоченном по стоимости виде вывести название и стоимость товаров от всех поставщиков, кроме поставщика 1.
+	SELECT ProductName, SupplierID, Price
+	FROM [Products]
+	WHERE 
+	NOT SupplierID=1
+	ORDER BY Price ASC 
+
+-- Задача 5. Вывести контактные имена клиентов, кроме тех, что из France и USA.
+	SELECT CustomerName,  Country
+	FROM [Customers]
+	WHERE	
+	Country NOT IN ('France', 'USA')
