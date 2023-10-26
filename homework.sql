e5347750f416ddd5d60c0551cf55d46a8836ea89
Tymofii Nikolenko <133020063+NIKOLENKO-TE@users.noreply.github.com>
2023-10-27 01:24:03 +0200

    Update homework.sql

--- homework.sql
+++ homework.sql
@@ -1,22 +1,26 @@
--- Задача 2. Вывести название и стоимость товаров от 20 EUR.
-	SELECT ProductName, Price
-	FROM [Products]
-	WHERE Price >20
-	ORDER BY Price ASC 
+-- Задача 1. Вывести среднюю реакцию по треку 1.
+db.reactions.aggregate([
+    {$match: {track_id: 1}},
+    {$group: {_id: null, average_reaction: { $avg: "$value" }}}
+])
 
--- Задача 3. Вывести страны поставщиков.
-	SELECT Country 
-	FROM [Suppliers]
+-- Задача 2. Увеличить баланс всех незаблокированных юзеров на 0.5%.
+db.users.updateMany(
+    { is_blocked: { $ne: true }},
+    { $mul: { balance: 1.005 }}
+)
 
--- Задача 4. В упорядоченном по стоимости виде вывести название и стоимость товаров от всех поставщиков, кроме поставщика 1.
-	SELECT ProductName, Price
-	FROM [Products]
-	WHERE 
-	NOT SupplierID=1
-	ORDER BY Price ASC 
+-- Задача 3. Вывести ко-во реакций с оценкой 4.
+db.reactions.aggregate([
+    {$match: {value: 4}},
+    { $count: "reaction_4_count"}
+])
 
--- Задача 5. Вывести контактные имена клиентов, кроме тех, что из France и USA.
-	SELECT CustomerName
-	FROM [Customers]
-	WHERE	
-	Country NOT IN ('France', 'USA')
+-- Задача 4. Вывести названия треков, принадлежащих юзеру 4.
+db.tracks.aggregate([
+    {$match: {author_id: 4}},
+    {$project: {title: 1, _id: 0 }}])
+)
+
+-- Задача 5. С помощью инструмента онлайн-проектирования опишите схему БД поликлиника
+https://dbdiagram.io/d/6538eeadffbf5169f06b7624
